7dab711dbeb0eefe607f7c38f8b8908d247626eb
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-06-26 17:16:25 -0300

    Style: add padding in Header component

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,9 +3,9 @@ import { BsCart3 } from "react-icons/bs";
 const Header = () => {
   return (
     <header className="w-full bg-primary">
-      <div className="flex items-center justify-between px-7 py-10">
-        <h2 className="text-3xl font-bold">CLUB CLOTHING</h2>
-        <div className="flex items-center gap-10 text-base font-semibold">
+      <div className="flex items-center justify-between px-[32px] py-10">
+        <h2 className="text-2xl font-bold">CLUB CLOTHING</h2>
+        <div className="flex items-center gap-10 text-sm font-semibold">
           <span>Explorar</span>
           <span>Login</span>
           <span className="flex items-center gap-1">
