399b694f55936e8a68a5b675daad9f6b619259b2
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-06-29 10:29:34 -0300

    Feat: add Custom Button to Login Page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,19 +1,27 @@
+import { FaGoogle } from "react-icons/fa";
+import CustomButton from "../components/custom-button";
+import { FiLogIn } from "react-icons/fi";
+
 const LoginPage = () => {
   return (
     <>
       <main className="flex h-[100%] items-center justify-center">
         <div className="flex w-[450px] flex-col items-center gap-4">
           <h2 className="text-xl font-bold text-black">Entre com sua conta</h2>
-          <button className="flex w-full items-center justify-center gap-2 rounded-xl bg-[#212529] p-[6px] text-sm font-semibold shadow-lg">
-            <span className="text-2xl">G</span>
-            <span>Entrar com o Google</span>
-          </button>
+
+          <CustomButton startIcon={<FaGoogle size={18} />}>
+            Entrar com o Google
+          </CustomButton>
 
           <span className="tex-sm font-semibold text-black">
             ou entre com seu e-mail
           </span>
 
-          <div className="h-[1px] w-full bg-[#212529]"> </div>
+          <div className="h-[1px] w-full bg-[#212529]"></div>
+
+          <div></div>
+
+          <CustomButton startIcon={<FiLogIn size={18} />}>Entrar</CustomButton>
         </div>
       </main>
     </>
