8008e8866a2c8a9f3f773c4c7c7daf651ca9301f
ayranoliveira1 <ayranoliveira1@gmail.com>
2024-06-29 11:17:29 -0300

    Feat: add final Custom button component

--- app/components/custom-button.tsx
+++ app/components/custom-button.tsx
@@ -5,9 +5,12 @@ interface CustomButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   startIcon?: React.ReactNode;
 }
 
-const CustomButton = ({ children, startIcon }: CustomButtonProps) => {
+const CustomButton = ({ children, startIcon, ...rest }: CustomButtonProps) => {
   return (
-    <button className="flex w-full items-center justify-center gap-2 rounded-xl bg-[#212529] p-[10px] text-sm font-semibold shadow-lg">
+    <button
+      {...rest}
+      className="flex w-full items-center justify-center gap-2 rounded-xl bg-[#212529] p-[10px] text-sm font-semibold shadow-lg"
+    >
       {startIcon && (
         <span className="flex h-full items-center">{startIcon}</span>
       )}
